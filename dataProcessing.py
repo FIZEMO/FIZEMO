78744ccc77560f2a46185ab870662d9ce67ac40a
Maciej Szarecki <mszarecki12@gmail.com>
2021-06-06 14:17:07 +0200

    FIZEMO-7-decimation method fix

--- dataProcessing.py
+++ dataProcessing.py
@@ -4,8 +4,8 @@ import scipy.signal as ss
 # function for signal decimation
 # first argument is signal data to decimation
 # second argument is frequency with which signal was sampled
-# third argument is modifier for how less samples we need for example value 10 means we want 10x less samples
-def decimation_process(signalData, samplingFrequency, decimationFrequency):
-    ratio = int(int(samplingFrequency) / int(decimationFrequency))
+# third argument is goal frequency with which signal should be sampled
+def decimation_process(signalData, samplingFrequency, goalFrequency):
+    ratio = int(int(samplingFrequency) / int(goalFrequency))
     decimatedSignal = ss.decimate(signalData, ratio, 8, axis=0)
     return decimatedSignal
