fa05574ae350c6d3228f04c77ca3afd1862bffd3
iwobr <Pulina0123>
2021-12-30 15:43:29 +0100

    Added simple script to process all signals at once.

---
+++ processSignals.py
@@ -0,0 +1,14 @@
+import os
+import main
+
+if __name__ == '__main__':
+    signal_files = os.listdir('D:\Projects\FIZEMO\signals')
+
+    for index, file in enumerate(signal_files):
+        os.chdir('D:\Projects\FIZEMO\signals')
+        os.rename(file, 'current.csv')
+        os.chdir('D:\Projects\FIZEMO')
+        main.main("./configuration/config.json")
+        os.chdir('D:\Projects\FIZEMO\signals')
+        new_name = "signal_{}.csv".format(index)
+        os.rename('current.csv', new_name)
