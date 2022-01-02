dab846b7df43255be5bcb8fc8fa82a0a334c68d8
Maciej Szarecki <mszarecki12@gmail.com>
2022-01-02 17:42:32 +0100

    merge files

--- processSignals.py
+++ processSignals.py
@@ -1,13 +1,50 @@
 import os
 import main
+import csv
 
 if __name__ == '__main__':
-    signal_files = os.listdir('D:\Projects\FIZEMO\signals')
+    signal_files = os.listdir('signals')
 
-    for index, file in enumerate(signal_files):
-        os.chdir('D:\Projects\FIZEMO\signals')
+    for file in signal_files:
+        os.chdir('D:\\instalki\\pg\\2 stopien\\sem1\\projekt badawczy\\git_hub\\FIZEMO\\signals')
         os.rename(file, 'current.csv')
-        os.chdir('D:\Projects\FIZEMO')
+        os.chdir('D:\\instalki\pg\\2 stopien\\sem1\\projekt badawczy\\git_hub\\FIZEMO')
         main.main("./configuration/config.json")
-        os.chdir('D:\Projects\FIZEMO\signals')
+        os.chdir('D:\\instalki\\pg\\2 stopien\\sem1\\projekt badawczy\\git_hub\\FIZEMO\\signals')
         os.rename('current.csv', file)
+
+
+    feature_files = os.listdir('results\\features')
+
+    with open("./results/merge/ECG_features.csv", 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        empty_destination_file = csv.reader(csv_file).line_num == 0
+        for file in feature_files:
+            with open("./results/features/"+file, 'r') as merge_file:
+                csv_reader = csv.reader(merge_file)
+                if file.__contains__('ECG') and empty_destination_file:
+                    empty_destination_file = False
+                    for index, row in enumerate(csv_reader):
+                        if index >= 6:
+                            csv_writer.writerow(row)
+                elif file.__contains__('ECG'):
+                    for index, row in enumerate(csv_reader):
+                        if index >= 7:
+                            csv_writer.writerow(row)
+
+    with open("./results/merge/GSR_features.csv", 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        empty_destination_file = csv.reader(csv_file).line_num == 0
+        for file in feature_files:
+            with open("./results/features/"+file, 'r') as merge_file:
+                csv_reader = csv.reader(merge_file)
+                if file.__contains__('GSR') and empty_destination_file:
+                    empty_destination_file = False
+                    for index, row in enumerate(csv_reader):
+                        if index >= 6:
+                            csv_writer.writerow(row)
+                elif file.__contains__('GSR'):
+                    for index, row in enumerate(csv_reader):
+                        if index >= 7:
+                            csv_writer.writerow(row)
+
